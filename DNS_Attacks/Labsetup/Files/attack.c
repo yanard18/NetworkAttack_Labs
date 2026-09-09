06a814b1a4f4fc95cd235bfc6a6e3c32b260bfce
yanard <yanard18@gmail.com>
2026-09-09 22:32:26 +0300

    update: uncomment attack.c

--- DNS_Attacks/Labsetup/Files/attack.c
+++ DNS_Attacks/Labsetup/Files/attack.c
@@ -8,7 +8,6 @@
 #define MAX_FILE_SIZE 1000000
 
 
-/* IP Header */
 struct ipheader {
   unsigned char      iph_ihl:4, //IP header length
                      iph_ver:4; //IP version
@@ -32,7 +31,6 @@ int main()
 {
   srand(time(NULL));
 
-  // Load the DNS request packet from file
   FILE * f_req = fopen("ip_req.bin", "rb");
   if (!f_req) {
      perror("Can't open 'ip_req.bin'");
@@ -42,7 +40,6 @@ int main()
   int n_req = fread(ip_req, 1, MAX_FILE_SIZE, f_req);
   fclose(f_req);
 
-  // Load the first DNS response packet from file
   FILE * f_resp = fopen("ip_resp.bin", "rb");
   if (!f_resp) {
      perror("Can't open 'ip_resp.bin'");
@@ -88,28 +85,20 @@ void send_dns_response(unsigned char *buf, int len)
   send_raw_packet((char *)buf, len);
 }
 
-/* Send the raw packet out 
- *    buffer: to contain the entire IP packet, with everything filled out.
- *    pkt_size: the size of the buffer.
- * */
 void send_raw_packet(char * buffer, int pkt_size)
 {
   struct sockaddr_in dest_info;
   int enable = 1;
 
-  // Step 1: Create a raw network socket.
   int sock = socket(AF_INET, SOCK_RAW, IPPROTO_RAW);
 
-  // Step 2: Set socket option.
   setsockopt(sock, IPPROTO_IP, IP_HDRINCL,
 	     &enable, sizeof(enable));
 
-  // Step 3: Provide needed information about destination.
   struct ipheader *ip = (struct ipheader *) buffer;
   dest_info.sin_family = AF_INET;
   dest_info.sin_addr = ip->iph_destip;
 
-  // Step 4: Send the packet out.
   sendto(sock, buffer, pkt_size, 0,
        (struct sockaddr *)&dest_info, sizeof(dest_info));
   close(sock);
